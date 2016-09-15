0885980513bddf5011514a3d0bc036b70b20c102
MORAN Hannah <m012384f@student.staffs.ac.uk>
2016-09-15 10:20:48 +0100

    ADD COINS!!

---
+++ Assets/Scripts/gameMaster.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class gameMaster : MonoBehaviour {
+
+	public int points;
+	public Text pointstext;
+
+	
+	// Update is called once per frame
+	void Update () {
+		pointstext.text = "Points: " + points.ToString();
+	}
+}
